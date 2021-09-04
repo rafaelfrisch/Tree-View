2884a67f49318e8797d8dd121e91d6e87fc70dae
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-09-03 21:24:43 -0300

    change background event

--- index.js
+++ index.js
@@ -18,5 +18,34 @@ function AddNode(parent){
         .append(AddContent(), AddIcons())
     $(parent).append(TreeNode)
 }
+
+function RemoveTreeNode(node){
+    console.log(node)
+}
+
+function TreeNodeClicked(node){
+    if(node.classList[0] == "tree-element")
+        $(node).addClass('tree-element-clicked')
+        if(node != ElementClicked)
+            $(ElementClicked).removeClass('tree-element-clicked')
+        ElementClicked = node
+
+    if(node.classList[0] == "fas")
+        if(node.classList[1] == "fa-plus")
+            console.log('add')
+        if(node.classList[1] == "fa-edit")
+            console.log('edit')
+        if(node.classList[1] == "fa-trash-alt")
+            console.log('remove')
+    return 
+}
+
 const Tree = document.getElementById('tree')
+let ElementClicked = false
 AddNode(Tree)
+AddNode(Tree)
+AddNode(Tree)
+
+$('.tree-element').click(event =>{
+    TreeNodeClicked(event.target)
+})
